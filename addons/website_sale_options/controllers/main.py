14c1ca4dc754dc65dae6f5b0c50a506da726fbe4
Ajay Javiya <aja@openerp.com>
2015-06-18 16:27:45 +0530

    [MIG] website_sale_options : migrate to new api

--- addons/website_sale_options/controllers/main.py
+++ addons/website_sale_options/controllers/main.py
@@ -1,42 +1,33 @@
 # -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp import SUPERUSER_ID
-from openerp.addons.web import http
-from openerp.addons.web.http import request
+from openerp import http
+from openerp.http import request
 from openerp.addons.website_sale.controllers.main import website_sale
 
-class website_sale_options(website_sale):
+class WebsiteSaleOptions(website_sale):
 
     @http.route(['/shop/product/<model("product.template"):product>'], type='http', auth="public", website=True)
     def product(self, product, category='', search='', **kwargs):
-        r = super(website_sale_options, self).product(product, category, search, **kwargs)
-
-        cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
-        template_obj = pool['product.template']
-
+        r = super(WebsiteSaleOptions, self).product(product, category, search, **kwargs)
         optional_product_ids = []
         for p in product.optional_product_ids:
-            ctx = dict(context, active_id=p.id)
-            optional_product_ids.append(template_obj.browse(cr, uid, p.id, context=ctx))
+            optional_product_ids.append(request.env['product.template'].with_context(active_id=p.id).browse(p.id))
 
         r.qcontext['optional_product_ids'] = optional_product_ids
         return r
 
     @http.route(['/shop/cart/update_option'], type='http', auth="public", methods=['POST'], website=True, multilang=False)
     def cart_options_update_json(self, product_id, add_qty=1, set_qty=0, goto_shop=None, lang=None, **kw):
-        cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
         if lang:
-            context = dict(context, lang=lang)
-            request.website = request.website.with_context(context)
+            request.website = request.website.with_context(lang=lang)
 
         order = request.website.sale_get_order(force_create=1)
-        product = pool['product.product'].browse(cr, uid, int(product_id), context=context)
-
-
-        option_ids = [p.id for tmpl in product.optional_product_ids for p in tmpl.product_variant_ids]
+        product = request.env['product.product'].browse(int(product_id))
+        options = product.optional_product_ids.mapped('product_variant_ids')
         optional_product_ids = []
         for k, v in kw.items():
-            if "optional-product-" in k and int(kw.get(k.replace("product", "add"))) and int(v) in option_ids:
+            if "optional-product-" in k and int(kw.get(k.replace("product", "add"))) and int(v) in option.ids:
                 optional_product_ids.append(int(v))
 
         value = {}
@@ -55,20 +46,19 @@ class website_sale_options(website_sale):
 
     @http.route(['/shop/modal'], type='json', auth="public", methods=['POST'], website=True)
     def modal(self, product_id, **kw):
-        cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
         pricelist = self.get_pricelist()
-        if not context.get('pricelist'):
+        context = {}
+        if not request.context.get('pricelist'):
             context['pricelist'] = int(pricelist)
 
         website_context = kw.get('kwargs', {}).get('context', {})
         context = dict(context or {}, **website_context)
-        from_currency = pool.get('product.price.type')._get_field_currency(cr, uid, 'list_price', context)
+        from_currency = request.env['product.price.type'].search([('field', '=', 'list_price')], limit=1)
         to_currency = pricelist.currency_id
-        compute_currency = lambda price: pool['res.currency']._compute(cr, uid, from_currency, to_currency, price, context=context)
-        product = pool['product.product'].browse(cr, uid, int(product_id), context=context)
-        request.website = request.website.with_context(context)
+        compute_currency = lambda price: request.env['res.currency']._compute(from_currency, to_currency, price)
+        product = request.env['product.product'].with_context(context).browse(int(product_id))
 
-        return request.website._render("website_sale_options.modal", {
+        return request.website.with_context(context)._render("website_sale_options.modal", {
                 'product': product,
                 'compute_currency': compute_currency,
                 'get_attribute_value_ids': self.get_attribute_value_ids,
