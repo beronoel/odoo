344ff2ac0768b40f602b54cbb122ec3a64b71f6c
Josse Colpaert <jco@odoo.com>
2015-06-29 16:13:07 +0200

    [IMP] Improve quants reconciliation with prefered domain when lots

--- addons/stock/stock.py
+++ addons/stock/stock.py
@@ -570,12 +570,11 @@ class stock_quant(osv.osv):
         """
         solving_quant = quant
         dom = [('qty', '<', 0)]
-        if quant.lot_id:
-            dom += ['|', ('lot_id', '=', quant.lot_id.id), ('lot_id', '=', False)] # Ideally lot in preferred domain
-        dom += [('owner_id', '=', quant.owner_id.id)]
-        dom += [('package_id', '=', quant.package_id.id)] #Could leave that one out also (makes it cleaner)
+        if quant.package_id.id:
+            dom += [('package_id', '=', quant.package_id.id)]
         dom += [('id', '!=', quant.propagated_from_id.id)]
-        quants = self.quants_get(cr, uid, quant.location_id, quant.product_id, quant.qty, dom, context=context)
+        quants = self.quants_get_prefered_domain(cr, uid, quant.location_id, quant.product_id, quant.qty, dom,
+                                                 restrict_lot_id=quant.lot_id.id, restrict_partner_id=quant.owner_id.id, context=context)
         product_uom_rounding = quant.product_id.uom_id.rounding
         for quant_neg, qty in quants:
             if not quant_neg or not solving_quant:
