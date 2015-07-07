b1b13f0c96e00b5b17e1e856200e44df2e7ec511
Paramjit Singh Sahota <psa@odoo.com>
2015-07-07 17:50:51 +0530

    [MIG] migrated res_bank into new api.

--- openerp/addons/base/res/res_bank.py
+++ openerp/addons/base/res/res_bank.py
@@ -1,162 +1,151 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.osv import fields, osv
-from openerp.tools.translate import _
+from openerp import api, fields, models, _
 from openerp.exceptions import UserError
 
-class Bank(osv.osv):
-    _description='Bank'
+
+class Bank(models.Model):
     _name = 'res.bank'
+    _description = 'Bank'
     _order = 'name'
-    _columns = {
-        'name': fields.char('Name', required=True),
-        'street': fields.char('Street'),
-        'street2': fields.char('Street2'),
-        'zip': fields.char('Zip', change_default=True),
-        'city': fields.char('City'),
-        'state': fields.many2one("res.country.state", 'Fed. State',
-            domain="[('country_id', '=', country)]"),
-        'country': fields.many2one('res.country', 'Country'),
-        'email': fields.char('Email'),
-        'phone': fields.char('Phone'),
-        'fax': fields.char('Fax'),
-        'active': fields.boolean('Active'),
-        'bic': fields.char('Bank Identifier Code',
-            help="Sometimes called BIC or Swift."),
-    }
-    _defaults = {
-        'active': lambda *a: 1,
-    }
-    def name_get(self, cr, uid, ids, context=None):
+
+    name = fields.Char(required=True)
+    street = fields.Char()
+    street2 = fields.Char()
+    zip = fields.Char(change_default=True)
+    city = fields.Char()
+    state = fields.Many2one("res.country.state", string='Fed. State',
+                            domain="[('country_id', '=', country)]")
+    country = fields.Many2one('res.country')
+    email = fields.Char()
+    phone = fields.Char()
+    fax = fields.Char()
+    active = fields.Boolean(default=True)
+    bic = fields.Char(string='Bank Identifier Code',
+                      help="Sometimes called BIC or Swift.")
+
+    @api.multi
+    def name_get(self):
         result = []
-        for bank in self.browse(cr, uid, ids, context):
+        for bank in self:
             result.append((bank.id, (bank.bic and (bank.bic + ' - ') or '') + bank.name))
         return result
 
-class res_partner_bank_type(osv.osv):
-    _description='Bank Account Type'
+
+class ResPartnerBankType(models.Model):
     _name = 'res.partner.bank.type'
+    _description = 'Bank Account Type'
     _order = 'name'
-    _columns = {
-        'name': fields.char('Name', required=True, translate=True),
-        'code': fields.char('Code', size=64, required=True),
-        'field_ids': fields.one2many('res.partner.bank.type.field', 'bank_type_id', 'Type Fields'),
-        'format_layout': fields.text('Format Layout', translate=True)
-    }
-    _defaults = {
-        'format_layout': lambda *args: "%(bank_name)s: %(acc_number)s"
-    }
 
-class res_partner_bank_type_fields(osv.osv):
-    _description='Bank type fields'
+    name = fields.Char(required=True, translate=True)
+    code = fields.Char(size=64, required=True)
+    field_ids = fields.One2many('res.partner.bank.type.field', 'bank_type_id',
+                                string='Type Fields')
+    format_layout = fields.Text(translate=True,
+                                default=lambda *args: "%(bank_name)s: %(acc_number)s")
+
+
+class ResPartnerBankTypeFields(models.Model):
     _name = 'res.partner.bank.type.field'
+    _description = 'Bank type fields'
     _order = 'name'
-    _columns = {
-        'name': fields.char('Field Name', required=True, translate=True),
-        'bank_type_id': fields.many2one('res.partner.bank.type', 'Bank Type', required=True, ondelete='cascade'),
-        'required': fields.boolean('Required'),
-        'readonly': fields.boolean('Readonly'),
-        'size': fields.integer('Max. Size'),
-    }
 
-class res_partner_bank(osv.osv):
+    name = fields.Char(string='Field Name', required=True, translate=True)
+    bank_type_id = fields.Many2one('res.partner.bank.type',
+                                   string='Bank Type',
+                                   required=True,
+                                   ondelete='cascade')
+    required = fields.Boolean()
+    readonly = fields.Boolean()
+    size = fields.Integer(string='Max. Size')
+
+
+class ResPartnerBank(models.Model):
     '''Bank Accounts'''
     _name = "res.partner.bank"
     _rec_name = "acc_number"
     _description = __doc__
     _order = 'sequence'
 
-    def _bank_type_get(self, cr, uid, context=None):
-        bank_type_obj = self.pool.get('res.partner.bank.type')
-
+    @api.multi
+    def _bank_type_get(self):
         result = []
-        type_ids = bank_type_obj.search(cr, uid, [])
-        bank_types = bank_type_obj.browse(cr, uid, type_ids, context=context)
-        for bank_type in bank_types:
+        for bank_type in self.env['res.partner.bank.type'].search([]):
             result.append((bank_type.code, bank_type.name))
         return result
 
-    def _default_value(self, cursor, user, field, context=None):
-        if context is None: context = {}
+    @api.model
+    def _default_value(self, field):
         if field in ('country_id', 'state_id'):
             value = False
         else:
             value = ''
-        if not context.get('address'):
+        if not self.env.context.get('address'):
             return value
 
-        for address in self.pool.get('res.partner').resolve_2many_commands(
-            cursor, user, 'address', context['address'], ['type', field], context=context):
-
+        for address in self.env['res.partner'].resolve_2many_commands('address',
+                                                                      self.env.context['address'],
+                                                                      ['type', field]):
             if address.get('type') == 'default':
                 return address.get(field, value)
             elif not address.get('type'):
                 value = address.get(field, value)
         return value
 
-    _columns = {
-        'acc_number': fields.char('Account Number', required=True),
-        'bank': fields.many2one('res.bank', 'Bank'),
-        'bank_bic': fields.char('Bank Identifier Code'),
-        'bank_name': fields.char('Bank Name'),
-        'owner_name': fields.char('Account Owner Name'),
-        'street': fields.char('Street'),
-        'zip': fields.char('Zip', change_default=True),
-        'city': fields.char('City'),
-        'country_id': fields.many2one('res.country', 'Country',
-            change_default=True),
-        'state_id': fields.many2one("res.country.state", 'Fed. State',
-            change_default=True, domain="[('country_id','=',country_id)]"),
-        'company_id': fields.many2one('res.company', 'Company',
-            ondelete='cascade', help="Only if this bank account belong to your company"),
-        'partner_id': fields.many2one('res.partner', 'Account Holder', ondelete='cascade', select=True, domain=['|',('is_company','=',True),('parent_id','=',False)]),
-        'state': fields.selection(_bank_type_get, 'Bank Account Type',
-            change_default=True),
-        'sequence': fields.integer('Sequence'),
-        'footer': fields.boolean("Display on Reports", help="Display this bank account on the footer of printed documents like invoices and sales orders."),
-        'currency_id': fields.many2one('res.currency', string='Currency', help="Currency of the bank account and its related journal."),
-    }
+    acc_number = fields.Char(string='Account Number', required=True)
+    bank = fields.Many2one('res.bank')
+    bank_bic = fields.Char(string='Bank Identifier Code')
+    bank_name = fields.Char()
+    owner_name = fields.Char(string='Account Owner Name',
+                             default=lambda self: self._default_value('name'))
+    street = fields.Char(default=lambda self: self._default_value('street'))
+    zip = fields.Char(change_default=True, default=lambda self: self._default_value('zip'))
+    city = fields.Char(default=lambda self: self._default_value('city'))
+    country_id = fields.Many2one('res.country', string='Country',
+                                 change_default=True,
+                                 default=lambda self: self._default_value('country_id'))
+    state_id = fields.Many2one("res.country.state", string='Fed. State',
+                               change_default=True, domain="[('country_id','=',country_id)]")
+    company_id = fields.Many2one('res.company', string='Company',
+                                 ondelete='cascade',
+                                 help="Only if this bank account belong to your company")
+    partner_id = fields.Many2one('res.partner', string='Account Holder',
+                                 ondelete='cascade', index=True,
+                                 domain=['|', ('is_company', '=', True), ('parent_id', '=', False)])
+    state = fields.Selection(_bank_type_get, string='Bank Account Type',
+                             change_default=True,
+                             default=lambda self: self._default_value('state_id'))
+    sequence = fields.Integer()
+    footer = fields.Boolean(string="Display on Reports",
+                            help="Display this bank account on the footer of printed documents like invoices and sales orders.")
+    currency_id = fields.Many2one('res.currency', string='Currency',
+                                  help="Currency of the bank account and its related journal.")
 
-    _defaults = {
-        'owner_name': lambda obj, cursor, user, context: obj._default_value(
-            cursor, user, 'name', context=context),
-        'street': lambda obj, cursor, user, context: obj._default_value(
-            cursor, user, 'street', context=context),
-        'city': lambda obj, cursor, user, context: obj._default_value(
-            cursor, user, 'city', context=context),
-        'zip': lambda obj, cursor, user, context: obj._default_value(
-            cursor, user, 'zip', context=context),
-        'country_id': lambda obj, cursor, user, context: obj._default_value(
-            cursor, user, 'country_id', context=context),
-        'state_id': lambda obj, cursor, user, context: obj._default_value(
-            cursor, user, 'state_id', context=context),
-        'name': '/',
-    }
-
-    def fields_get(self, cr, uid, allfields=None, context=None, write_access=True, attributes=None):
-        res = super(res_partner_bank, self).fields_get(cr, uid, allfields=allfields, context=context, write_access=write_access, attributes=attributes)
-        bank_type_obj = self.pool.get('res.partner.bank.type')
-        type_ids = bank_type_obj.search(cr, uid, [])
-        types = bank_type_obj.browse(cr, uid, type_ids)
-        for type in types:
+    @api.model
+    def fields_get(self, allfields=None, context=None, write_access=True, attributes=None):
+        res = super(ResPartnerBank, self).fields_get(allfields=allfields,
+                                                     context=self.env.context,
+                                                     write_access=write_access,
+                                                     attributes=attributes)
+        for type in self.env['res.partner.bank.type'].search([]):
             for field in type.field_ids:
                 if field.name in res:
                     res[field.name].setdefault('states', {})
-                    res[field.name]['states'][type.code] = [
-                            ('readonly', field.readonly),
-                            ('required', field.required)]
+                    res[field.name]['states'][type.code] = [('readonly', field.readonly),
+                                                            ('required', field.required)]
         return res
 
-    def _prepare_name_get(self, cr, uid, bank_dicts, context=None):
+    @api.model
+    def _prepare_name_get(self, bank_dicts):
         """ Format the name of a res.partner.bank.
             This function is designed to be inherited to add replacement fields.
             :param bank_dicts: a list of res.partner.bank dicts, as returned by the method read()
             :return: [(id, name), ...], as returned by the method name_get()
         """
         # prepare a mapping {code: format_layout} for all bank types
-        bank_type_obj = self.pool.get('res.partner.bank.type')
-        bank_types = bank_type_obj.browse(cr, uid, bank_type_obj.search(cr, uid, []), context=context)
+        BankType = self.env['res.partner.bank.type']
+        bank_types = BankType.search([])
         bank_code_format = dict((bt.code, bt.format_layout) for bt in bank_types)
 
         res = []
@@ -171,45 +160,28 @@ class res_partner_bank(osv.osv):
                 except Exception:
                     raise UserError(_("Bank account name formating error") + ': ' + _("Check the format_layout field set on the Bank Account Type."))
             if data.get('currency_id'):
-                currency_name = self.pool.get('res.currency').browse(cr, uid, data['currency_id'][0], context=context).name
+                currency_name = self.env['res.currency'].browse(data['currency_id'][0]).name
                 name += ' (' + currency_name + ')'
             res.append((data.get('id', False), name))
         return res
 
-    def name_get(self, cr, uid, ids, context=None):
-        if not len(ids):
-            return []
-        bank_dicts = self.read(cr, uid, ids, self.fields_get_keys(cr, uid, context=context), context=context)
-        return self._prepare_name_get(cr, uid, bank_dicts, context=context)
+    @api.multi
+    def name_get(self):
+        bank_dicts = self.read(self.fields_get_keys())
+        return self._prepare_name_get(bank_dicts)
 
-    def onchange_company_id(self, cr, uid, ids, company_id, context=None):
-        result = {}
-        if company_id:
-            c = self.pool.get('res.company').browse(cr, uid, company_id, context=context)
-            if c.partner_id:
-                r = self.onchange_partner_id(cr, uid, ids, c.partner_id.id, context=context)
-                r['value']['partner_id'] = c.partner_id.id
-                r['value']['footer'] = 1
-                result = r
-        return result
+    @api.onchange('company_id')
+    def onchange_company_id(self):
+        if self.company_id and self.company_id.partner_id:
+            self.partner_id = self.company_id.partner_id.id
+            self.footer = 1
 
-    def onchange_bank_id(self, cr, uid, ids, bank_id, context=None):
-        result = {}
-        if bank_id:
-            bank = self.pool.get('res.bank').browse(cr, uid, bank_id, context=context)
-            result['bank_name'] = bank.name
-            result['bank_bic'] = bank.bic
-        return {'value': result}
-
-    def onchange_partner_id(self, cr, uid, ids, partner_id, context=None):
-        result = {}
-        if partner_id is not False:
-            # be careful: partner_id may be a NewId
-            part = self.pool['res.partner'].browse(cr, uid, [partner_id], context=context)
-            result['owner_name'] = part.name
-            result['street'] = part.street or False
-            result['city'] = part.city or False
-            result['zip'] = part.zip or False
-            result['country_id'] = part.country_id.id
-            result['state_id'] = part.state_id.id
-        return {'value': result}
+    @api.onchange('partner_id')
+    def onchange_partner_id(self):
+        if self.partner_id is not False:
+            self.owner_name = self.partner_id.name
+            self.street = self.partner_id.street or False
+            self.city = self.partner_id.city or False
+            self.zip = self.partner_id.zip or False
+            self.country_id = self.partner_id.country_id.id
+            self.state_id = self.partner_id.state_id.id
