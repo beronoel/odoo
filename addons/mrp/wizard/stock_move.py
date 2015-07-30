74509866f216ae52aa21d7f083d1aa86146125d5
Dhaval Panchal <dpa@odoo.com>
2015-07-30 11:14:56 +0530

    [IMP] mrp: migrate as per newapi guideline.

--- addons/mrp/wizard/stock_move.py
+++ addons/mrp/wizard/stock_move.py
@@ -1,28 +1,26 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from openerp.osv import fields, osv
-from openerp.tools.translate import _
+from openerp import api, fields, models
 import openerp.addons.decimal_precision as dp
 
-class stock_move_consume(osv.osv_memory):
+
+class StockMoveConsume(models.TransientModel):
     _name = "stock.move.consume"
     _description = "Consume Products"
 
-    _columns = {
-        'product_id': fields.many2one('product.product', 'Product', required=True, select=True),
-        'product_qty': fields.float('Quantity', digits_compute=dp.get_precision('Product Unit of Measure'), required=True),
-        'product_uom': fields.many2one('product.uom', 'Product Unit of Measure', required=True),
-        'location_id': fields.many2one('stock.location', 'Location', required=True),
-        'restrict_lot_id': fields.many2one('stock.production.lot', 'Lot'),
-    }
+    product_id = fields.Many2one('product.product', string='Product', required=True, select=True)
+    product_qty = fields.Float(string='Quantity', digits_compute=dp.get_precision('Product Unit of Measure'), required=True)
+    product_uom = fields.Many2one('product.uom', string='Product Unit of Measure', required=True)
+    location_id = fields.Many2one('stock.location', string='Location', required=True)
+    restrict_lot_id = fields.Many2one('stock.production.lot', string='Lot')
 
     #TOFIX: product_uom should not have different category of default UOM of product. Qty should be convert into UOM of original move line before going in consume and scrap
-    def default_get(self, cr, uid, fields, context=None):
-        if context is None:
-            context = {}
-        res = super(stock_move_consume, self).default_get(cr, uid, fields, context=context)
-        move = self.pool.get('stock.move').browse(cr, uid, context['active_id'], context=context)
+    @api.model
+    def default_get(self, fields):
+
+        res = super(StockMoveConsume, self).default_get(fields)
+        move = self.env['stock.move'].browse(self._context['active_id'])
         if 'product_id' in fields:
             res.update({'product_id': move.product_id.id})
         if 'product_uom' in fields:
@@ -33,19 +31,13 @@ class stock_move_consume(osv.osv_memory):
             res.update({'location_id': move.location_id.id})
         return res
 
-
-
-    def do_move_consume(self, cr, uid, ids, context=None):
-        if context is None:
-            context = {}
-        move_obj = self.pool.get('stock.move')
-        uom_obj = self.pool.get('product.uom')
-        move_ids = context['active_ids']
-        for data in self.browse(cr, uid, ids, context=context):
+    @api.multi
+    def do_move_consume(self):
+        MoveObj = self.env['stock.move']
+        move_ids = self._context['active_ids']
+        for data in self:
             if move_ids and move_ids[0]:
-                move = move_obj.browse(cr, uid, move_ids[0], context=context)
-            qty = uom_obj._compute_qty(cr, uid, data['product_uom'].id, data.product_qty, data.product_id.uom_id.id)
-            move_obj.action_consume(cr, uid, move_ids,
-                             qty, data.location_id.id, restrict_lot_id=data.restrict_lot_id.id,
-                             context=context)
+                move = MoveObj.browse(move_ids[0])
+            qty = self.env['product.uom']._compute_qty(data['product_uom'].id, data.product_qty, data.product_id.uom_id.id)
+            MoveObj.action_consume(move_ids, qty, data.location_id.id, restrict_lot_id=data.restrict_lot_id.id)
         return {'type': 'ir.actions.act_window_close'}
