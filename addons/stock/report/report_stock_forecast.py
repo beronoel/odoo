11c4decaf2defed2608fc7a7bb92e1994404c112
Josse Colpaert <jco@odoo.com>
2015-08-19 10:54:09 +0200

    [IMP] But dates in the past in current date

--- addons/stock/report/report_stock_forecast.py
+++ addons/stock/report/report_stock_forecast.py
@@ -32,21 +32,9 @@ class report_stock_forecast(models.Model):
         (SELECT
             MIN(sq.id) as id,
             sq.product_id,
-            CASE WHEN sm.date < CURRENT_DATE
-            THEN date_trunc('week', to_date(to_char(sm.date, 'YYYY/MM/DD'), 'YYYY/MM/DD'))
-            ELSE date_trunc('week', to_date(to_char(CURRENT_DATE, 'YYYY/MM/DD'), 'YYYY/MM/DD'))
-            END AS date,
+            date_trunc('week', to_date(to_char(CURRENT_DATE, 'YYYY/MM/DD'), 'YYYY/MM/DD')) as date,
             SUM(sq.qty) AS product_qty
             FROM
-            (SELECT min(date_expected) AS date FROM stock_move AS sm
-                LEFT JOIN
-                    stock_location source_location ON sm.location_id = source_location.id
-                LEFT JOIN
-                    stock_location dest_location ON sm.location_dest_id = dest_location.id
-                WHERE
-                    sm.state IN ('confirmed','assigned','waiting') AND
-                    (dest_location.usage = 'internal' AND source_location.usage != 'internal')
-                        or (source_location.usage = 'internal' AND dest_location.usage != 'internal')) AS sm,
             stock_quant as sq
             LEFT JOIN
             product_product ON product_product.id = sq.product_id
@@ -59,7 +47,10 @@ class report_stock_forecast(models.Model):
             SELECT
             MIN(-sm.id) as id,
             sm.product_id,
-            date_trunc('week', to_date(to_char(sm.date_expected, 'YYYY/MM/DD'), 'YYYY/MM/DD')) AS date,
+            CASE WHEN sm.date_expected > CURRENT_DATE
+            THEN date_trunc('week', to_date(to_char(sm.date_expected, 'YYYY/MM/DD'), 'YYYY/MM/DD'))
+            ELSE date_trunc('week', to_date(to_char(CURRENT_DATE, 'YYYY/MM/DD'), 'YYYY/MM/DD')) END
+            AS date,
             SUM(sm.product_qty) AS product_qty
             FROM
                stock_move as sm
@@ -77,7 +68,10 @@ class report_stock_forecast(models.Model):
             SELECT
                 MIN(-sm.id) as id,
                 sm.product_id,
-                date_trunc('week', to_date(to_char(sm.date_expected, 'YYYY/MM/DD'), 'YYYY/MM/DD')) AS date,
+                CASE WHEN sm.date_expected > CURRENT_DATE
+                    THEN date_trunc('week', to_date(to_char(sm.date_expected, 'YYYY/MM/DD'), 'YYYY/MM/DD'))
+                    ELSE date_trunc('week', to_date(to_char(CURRENT_DATE, 'YYYY/MM/DD'), 'YYYY/MM/DD')) END
+                AS date,
                 SUM(-(sm.product_qty)) AS product_qty
             FROM
                stock_move as sm
@@ -105,9 +99,9 @@ class report_stock_forecast(models.Model):
              LEFT JOIN
              stock_location dest_location ON sm.location_dest_id = dest_location.id
              WHERE
-             sm.state IN ('confirmed','assigned','waiting') and
-             (dest_location.usage = 'internal' AND source_location.usage != 'internal')
-              or (source_location.usage = 'internal' AND dest_location.usage != 'internal')) AS DATE_SEARCH)
+             sm.state IN ('confirmed','assigned','waiting') and sm.date_expected > CURRENT_DATE and
+             ((dest_location.usage = 'internal' AND source_location.usage != 'internal')
+              or (source_location.usage = 'internal' AND dest_location.usage != 'internal'))) AS DATE_SEARCH)
              SUB ON (SUB.date IS NOT NULL)
     GROUP BY MAIN.product_id,SUB.date, MAIN.date
     ) AS FINAL
