f4986ea02c551a778ee0cc5d1124351cd1992e43
Josse Colpaert <jco@odoo.com>
2015-09-15 16:06:03 +0200

    [IMP] Change inherited action_confirm method by the one creating procurements + start changing procurement_jit explanation

--- addons/procurement_jit/__openerp__.py
+++ addons/procurement_jit/__openerp__.py
@@ -7,18 +7,11 @@
     'version': '1.0',
     'category': 'Base',
     'description': """
-This module allows Just In Time computation of procurement orders.
-==================================================================
+This module will automatically reserve the different pickings
+=============================================================
+In order to 
 
-If you install this module, you will not have to run the regular procurement
-scheduler anymore (but you still need to run the minimum order point rule
-scheduler, or for example let it run daily).
-All procurement orders will be processed immediately, which could in some
-cases entail a small performance impact.
 
-It may also increase your stock size because products are reserved as soon
-as possible and the scheduler time range is not taken into account anymore.
-In that case, you can not use priorities any more on the different picking.
     """,
     'website': 'https://www.odoo.com/page/manufacturing',
     'depends': ['sale_stock'],
--- addons/procurement_jit/sale.py
+++ addons/procurement_jit/sale.py
@@ -7,8 +7,12 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     @api.multi
-    def action_confirm(self):
-        res = super(SaleOrder, self).action_confirm()
+    def _action_procurement_create(self):
+        res = super(SaleOrder, self)._action_procurement_create()
         for order in self:
             order.picking_ids.filtered(lambda x: x.state=='confirmed').action_assign()
+            reassign = order.picking_ids.filtered(lambda x: x.state=='partially_available')
+            if reassign:
+                reassign.do_unreserve()
+                reassign.action_assign()
         return res
