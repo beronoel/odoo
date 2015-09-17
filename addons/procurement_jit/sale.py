45bf4d366bc5bd0f6157589c4721fe8d4b8808f1
Josse Colpaert <jco@odoo.com>
2015-09-17 16:02:57 +0200

    [IMP] ()

--- addons/procurement_jit/sale.py
+++ addons/procurement_jit/sale.py
@@ -12,7 +12,7 @@ class SaleOrderLine(models.Model):
         orders = list(set(x.order_id for x in self))
         for order in orders:
             order.picking_ids.filtered(lambda x: x.state=='confirmed' and not x.quant_reserved_exist).action_assign()
-            reassign = order.picking_ids.filtered(lambda x: not x.printed and x.state=='partially_available' or ((x.state=='confirmed') and x.quant_reserved_exist))
+            reassign = order.picking_ids.filtered(lambda x: not x.printed and (x.state=='partially_available' or ((x.state=='confirmed') and x.quant_reserved_exist)))
             if reassign:
                 reassign.do_unreserve()
                 reassign.action_assign()
