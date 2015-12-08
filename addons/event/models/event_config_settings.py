ecb68b0d5cead6d04559ef253dc5b0ff8a3073fc
Virbhadrasinh Gohil <vgo@openerp.com>
2015-12-09 01:14:14 +0530

    [ADD] event: Added mail template which is send when change event schedule(Event name, Location, Start date, End date) and also when cancel.

--- addons/event/models/event_config_settings.py
+++ addons/event/models/event_config_settings.py
@@ -1,16 +1,19 @@
 # -*- coding: utf-8 -*-
 
-from openerp.osv import fields, osv
+from openerp.osv import fields, osv 
 
 class event_config_settings(osv.TransientModel):
     _name='event.config.settings'
     _inherit='res.config.settings'
     _columns = {
-        'module_event_sale': fields.selection([
-            (0, "All events are free"),
-            (1, 'Allow selling tickets')
+        'event_config_type': fields.selection([
+            (1, 'All events are free'),
+            (2, 'Allow selling tickets'),
+            (3, 'Allow your customer to buy tickets from your eCommerce'),
             ], "Tickets",
-            help='Install the event_sale module'),
+            help='Install website_event_sale or event_sale module based on options'),
+        'module_event_sale': fields.boolean(),
+        'module_website_event_sale': fields.boolean(),
         'module_website_event_track': fields.selection([
             (0, "No mini website per event"),
             (1, 'Allow tracks, agenda and dedicated menus/website per event')
@@ -30,11 +33,32 @@ class event_config_settings(osv.TransientModel):
             (0, "No automated emails"),
             (1, 'Schedule emails to attendees and subscribers')
             ], "Email Scheduling",
-            help='You will be able to configure emails, and to schedule them to be automatically sent to the attendees on registration and/or attendance',
+            help='You will be able to configure emails, and to schedule them to be automatically sent to the attendees on subscription and/or attendance',
             implied_group='event.group_email_scheduling'),            
         'module_event_barcode': fields.boolean("Scan badges to confirm attendances",
             help="Install the event_barcode module"),
     }
+
+    def default_get(self, cr, uid, fields, context=None):
+        res = super(event_config_settings, self).default_get(cr, uid, fields, context)
+        if 'event_config_type' in fields: res['event_config_type'] = 1
+        if 'module_website_event_sale' in fields and res['module_website_event_sale']:
+            res['event_config_type'] = 3
+        elif 'module_event_sale' in fields and res['module_event_sale']:
+            res['event_config_type'] = 2
+        return res
+
+    def onchange_event_config_type(self, cr, uid, ids, event_config_type, context=None):
+        if event_config_type == 3:
+            return {'value': {'module_website_event_sale': True}}
+        elif event_config_type == 2:
+            return {'value': {'module_event_sale': True, 'module_website_event_sale': False}}
+        return {'value': {'module_event_sale': False, 'module_website_event_sale': False}}
+
+
+    def set_default_event_config_type(self, cr, uid, ids, context=None):
+        config_value = self.browse(cr, uid, ids, context=context).event_config_type
+        self.pool['ir.values'].set_default(cr, uid, 'event.config.settings', 'event_config_type', config_value)
 
     def set_default_auto_confirmation(self, cr, uid, ids, context=None):
         config_value = self.browse(cr, uid, ids, context=context).auto_confirmation
