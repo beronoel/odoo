84d075a316b7e2981da52ffae3735aad69969368
Yannick Tivisse <yti@odoo.com>
2015-12-24 10:28:35 +0100

    ****ing fix

--- addons/sale_mrp/tests/test_move_explode.py
+++ addons/sale_mrp/tests/test_move_explode.py
@@ -18,7 +18,6 @@ class TestMoveExplode(common.TransactionCase):
         self.product_bom = self.env.ref('product.product_product_5')
         #bom with that product
         self.bom = self.env.ref('mrp.mrp_bom_kit')
-        self.bom.write({'property_ids': [(6,0,[])]})
         #partner agrolait
         self.partner = self.env.ref('base.res_partner_1')
         #bom:  (with property: DDR 512MB)
@@ -50,31 +49,26 @@ class TestMoveExplode(common.TransactionCase):
     def test_00_sale_move_explode(self):
         """check that when creating a sale order with a product that has a phantom BoM, move explode into content of the
             BoM"""
-        #create sale order with one sale order line containing product with a phantom bom
+        # Remove properties on the bom an on the bom lines
+        self.bom.write({'property_ids': [(6,0,[])]})
+        for line in self.bom.bom_line_ids:
+            line.write({'property_ids': [(6,0,[])]})
+        # Create sale order with one sale order line containing product with a phantom bom
         so_vals = {
             'partner_id': self.partner.id,
             'partner_invoice_id': self.partner.id,
             'partner_shipping_id': self.partner.id,
+            'order_line': [(0, 0, {'name': self.product_bom.name, 'product_id': self.product_bom.id, 'product_uom_qty': 1, 'product_uom': self.product_bom.uom_id.id})],
             'pricelist_id': self.pricelist.id,
         }
         self.so = self.SaleOrder.create(vals=so_vals)
-        sol_vals = {
-            'order_id': self.so.id,
-            'name': self.product_bom.name,
-            'product_id': self.product_bom.id,
-            'product_uom': self.product_bom.uom_id.id,
-            'product_uom_qty': 1.0,
-        }
-        self.SaleOrderLine.create(values=sol_vals)
         #confirm sale order
         self.so.action_confirm()
         #get all move associated to that sale_order
         move_ids = self.so.picking_ids.mapped('move_lines').ids
         #we should have same amount of move as the component in the phatom bom
         bom_component_length = self.MrpBom._bom_explode(self.bom, self.product_bom, 1.0, properties=[])
-        # yti todo : fix me
-        # import pdb; pdb.set_trace()
-        # self.assertEqual(len(move_ids), len(bom_component_length[0]))
+        self.assertEqual(len(move_ids), len(bom_component_length[0]))
 
     def test_00_bom_find(self):
         """Check that _bom_find searches the bom corresponding to the properties passed or takes the bom with the smallest
