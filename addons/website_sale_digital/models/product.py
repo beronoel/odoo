badb38c49fb60dfc2773ced98e04e93883c44969
Jérome Maes <jem@openerp.com>
2016-04-14 15:12:36 +0200

    [IMP] website_sale_digital : use read_group for compted fields

--- addons/website_sale_digital/models/product.py
+++ addons/website_sale_digital/models/product.py
@@ -1,19 +1,23 @@
-# -*- encoding: utf-8 -*-
-from openerp import models, fields, api, _
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
 
 
 class ProductTemplate(models.Model):
+
     _inherit = ['product.template']
 
     attachment_count = fields.Integer(compute='_compute_attachment_count', string="File")
 
     @api.multi
     def _compute_attachment_count(self):
-        IrAttachment = self.env['ir.attachment']
-        for ptemplate in self:
-            prod_tmpl_attach_count = IrAttachment.search_count([('res_model', '=', 'product.template'), ('res_id', 'in', ptemplate.ids)])
-            prod_attach_count = IrAttachment.search_count([('res_model', '=', 'product.product'), ('res_id', 'in', ptemplate.product_variant_ids.ids)])
-            ptemplate.attachment_count = prod_tmpl_attach_count + prod_attach_count
+        Attachment = self.env['ir.attachment']
+        attachment_data = Attachment.read_group([('res_model', '=', self._name), ('res_id', 'in', self.ids)], ['res_id'], ['res_id'])
+        mapped_data = dict([(data['res_id'][0], data['res_id_count']) for data in attachment_data])
+        for product_template in self:
+            prod_attach_count = Attachment.search_count([('res_model', '=', 'product.product'), ('res_id', 'in', product_template.product_variant_ids.ids)])
+            product_template.attachment_count = mapped_data.get(product_template.id, 0) + prod_attach_count
 
     @api.model
     def _get_product_template_type(self):
@@ -39,15 +43,17 @@ class ProductTemplate(models.Model):
 
 
 class Product(models.Model):
+
     _inherit = 'product.product'
 
     attachment_count = fields.Integer(compute='_compute_attachment_count', string="File")
 
     @api.multi
     def _compute_attachment_count(self):
-        IrAttachment = self.env['ir.attachment']
+        attachment_data = self.env['ir.attachment'].read_group([('res_model', '=', self._name), ('res_id', 'in', self.ids)], ['res_id'], ['res_id'])
+        mapped_data = dict([(data['res_id'][0], data['res_id_count']) for data in attachment_data])
         for product in self:
-            product.attachment_count = IrAttachment.search_count([('res_model', '=', product._name), ('res_id', 'in', product.ids)])
+            product.attachment_count = mapped_data.get(product.id, 0)
 
     @api.multi
     def action_open_attachments(self):
