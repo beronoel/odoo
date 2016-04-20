f25b02329f82a9f96fed1266c285f1837abcfc31
Géry Debongnie <ged@odoo.com>
2016-04-20 15:18:20 +0200

    Revert "[FIX] web_kanban: correct count for dataset size"
    This reverts commit 9b54b042a03564b8f808b0fff292573a73ef3044.

--- addons/web_kanban/static/src/js/kanban_view.js
+++ addons/web_kanban/static/src/js/kanban_view.js
@@ -138,7 +138,6 @@ var KanbanView = View.extend({
                     }));
                 } else {
                     new_ids = _.pluck(data.records, 'id');
-                    self.dataset._length = data.count;
                 }
                 self.dataset.alter_ids(new_ids);
                 self.data = data;
@@ -158,20 +157,17 @@ var KanbanView = View.extend({
 
     load_records: function (offset, dataset) {
         dataset = dataset || this.dataset;        
-        var query = dataset._model.query(this.fields_keys.concat(['__last_update']))
+        return dataset._model.query(this.fields_keys.concat(['__last_update']))
                 .limit(this.limit || false)
-                .offset(offset || 0);
-        return query.all()
-            .then(function (records) {
-                return query.count().then(function(count) {
+                .offset(offset || 0)
+                .all()
+                .then(function (records) {
                     return {
                         records: records,
                         is_empty: !records.length,
                         grouped: false,
-                        count: count,
                     };
-                })
-            });
+                });
     },
 
     load_groups: function (options) {
