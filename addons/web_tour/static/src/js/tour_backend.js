b040d1f14f60cc796dc766d32df2f36f48603331
Géry Debongnie <ged@odoo.com>
2016-05-27 11:49:49 +0200

    wip

--- addons/web_tour/static/src/js/tour_backend.js
+++ addons/web_tour/static/src/js/tour_backend.js
@@ -1,38 +1,25 @@
 odoo.define('web_tour.tour', function(require) {
 "use strict";
 
-var core = require('web.core');
 var Tour = require('web_tour.Tour');
-
-// select the target node
-// var target = document.body;
- 
-// create an observer instance
-// var observer = new MutationObserver(function(mutations) {
-//   mutations.forEach(function(mutation) {
-//     console.log(mutation.type);
-//   });    
-// });
- 
-// configuration of the observer:
- 
-// pass in the target node, as well as the observer options
- 
-// later, you can stop observing
-// observer.disconnect();
-
 
 Tour.include({
     init: function() {
         this._super();
-        // core.bus.on('DOM_updated', this, this.check_for_tooltip);
-        // core.bus.on('click', this, this.check_for_tooltip);
-        var observer = new MutationObserver(this.check_for_tooltip.bind(this));
-        var config = { attributes: true, childList: true, characterData: true };
-        document.addEventListener("DOMContentLoaded", function(event) {
-            observer.observe(document.body, config);
-        });
+        if (document.body) {
+            this.observe();
+        } else {
+            document.addEventListener("DOMContentLoaded", this.observe.bind(this));
+        }
     },
+    observe: function () {
+        var check_tooltip = _.throttle(this.check_for_tooltip.bind(this), 100, {leading: false});
+        var observer = new MutationObserver(check_tooltip);
+        observer.observe(document.body, {
+            childList: true,
+            characterData: true
+        });
+    }
 });
 
 return new Tour();
