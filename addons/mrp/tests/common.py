ac3ff7024775383ba0eb63c14669118567b94483
Thibault Delavallée <tde@openerp.com>
2016-06-03 17:10:23 +0200

    [TESTS] product, mrp: udpate data + bom tests

--- addons/mrp/tests/common.py
+++ addons/mrp/tests/common.py
@@ -13,6 +13,11 @@ class TestMrpCommon(common2.TestStockCommon):
         user_group_mrp_user = cls.env.ref('mrp.group_mrp_user')
         user_group_mrp_manager = cls.env.ref('mrp.group_mrp_manager')
 
+        # Update demo products
+        (cls.product_1 | cls.product_2 | cls.product_3 | cls.product_4 | cls.product_4).write({
+            'type': 'product',
+        })
+
         # User Data: mrp user and mrp manager
         Users = cls.env['res.users'].with_context({'no_reset_password': True, 'mail_create_nosubscribe': True})
         cls.user_mrp_user = Users.create({
@@ -39,40 +44,47 @@ class TestMrpCommon(common2.TestStockCommon):
             'name': 'Line I',
         })
         cls.operation_1 = cls.env['mrp.routing.workcenter'].create({
-            'name': 'Assembly',
+            'name': 'Machine A',
             'workcenter_id': cls.workcenter_1.id,
             'routing_id': cls.routing_1.id,
-            'time_cycle': 60,
+            'time_cycle': 20,
+            'sequence': 1,
+        })
+        cls.operation_2 = cls.env['mrp.routing.workcenter'].create({
+            'name': 'Machine B',
+            'workcenter_id': cls.workcenter_1.id,
+            'routing_id': cls.routing_1.id,
+            'time_cycle': 10,
             'sequence': 2,
         })
 
         cls.bom_1 = cls.env['mrp.bom'].create({
             'product_id': cls.product_1.id,
             'product_tmpl_id': cls.product_1.product_tmpl_id.id,
-            'product_uom_id': cls.product_1.uom_id.id,
+            'product_uom_id': cls.uom_dozen.id,
             'product_qty': 2.0,
             'routing_id': cls.routing_1.id,
             'type': 'normal',
             'bom_line_ids': [
                 (0, 0, {'product_id': cls.product_2.id, 'product_qty': 1}),
-                (0, 0, {'product_id': cls.product_3.id, 'product_qty': 4})
+                (0, 0, {'product_id': cls.product_3.id, 'product_qty': 2})
             ]})
         cls.bom_2 = cls.env['mrp.bom'].create({
             'product_id': cls.product_3.id,
             'product_tmpl_id': cls.product_3.product_tmpl_id.id,
             'product_uom_id': cls.product_3.uom_id.id,
-            'product_qty': 3.0,
+            'product_qty': 4.0,
             'routing_id': cls.routing_1.id,
             'type': 'phantom',
             'bom_line_ids': [
-                (0, 0, {'product_id': cls.product_4.id, 'product_qty': 4}),
-                (0, 0, {'product_id': cls.product_5.id, 'product_qty': 6})
+                (0, 0, {'product_id': cls.product_4.id, 'product_qty': 6}),
+                (0, 0, {'product_id': cls.product_5.id, 'product_qty': 3})
             ]})
 
         cls.production_1 = cls.env['mrp.production'].create({
             'name': 'MO-Test',
             'product_id': cls.product_1.id,
-            'product_qty': 2.0,
+            'product_qty': 48,
             'bom_id': cls.bom_1.id,
             'product_uom_id': cls.product_1.uom_id.id,
         })
--- addons/mrp/tests/test_bom.py
+++ addons/mrp/tests/test_bom.py
@@ -7,23 +7,12 @@ from odoo.addons.mrp.tests.common import TestMrpCommon
 class TestBoM(TestMrpCommon):
 
     def test_basic(self):
-        # make the production order
-        production = self.production_1
-
-        # # compute production order data
-        # production.action_compute()
-
-        # # confirm production
-        # production.signal_workflow('button_confirm')
-        self.assertEqual(production.state, 'confirmed')
-
-        # reserve product
-        # production.force_production()
+        self.assertEqual(self.production_1.state, 'confirmed')
 
         # produce product
         produce_wizard = self.env['mrp.product.produce'].with_context({
-            'active_id': production.id,
-            'active_ids': [production.id],
+            'active_id': self.production_1.id,
+            'active_ids': [self.production_1.id],
         }).create({
             'product_qty': 1.0,
         })
@@ -35,10 +24,93 @@ class TestBoM(TestMrpCommon):
 
     def test_explode(self):
         res = self.bom_1.explode_new(3)
-        print '--------'
-        print res
+        # print '--------'
+        # print res
 
     def test_explode_from_order(self):
-        res = self.production_1.button_plan()
-        print '--------'
-        print res
+        # reset quantities
+        self.env['stock.change.product.qty'].create({
+            'product_id': self.product_1.id,
+            'new_quantity': 0.0,
+            'location_id': self.warehouse_1.lot_stock_id.id,
+        }).change_product_qty()
+
+        (self.product_2 | self.product_4).write({
+            'tracking': 'lot',
+        })
+        # assign consume material
+        self.production_1.action_assign()
+        self.assertEqual(self.production_1.availability, 'waiting')
+
+        # check consume materials of manufacturing order
+        self.assertEqual(len(self.production_1.move_raw_ids), 3)
+        # for move in self.production_1.move_raw_ids:
+        #     print move.name, move.state, move.product_id, move.product_qty, move.product_uom_qty, move.unit_factor
+        product_2_consume_moves = self.production_1.move_raw_ids.filtered(lambda x: x.product_id == self.product_2)
+        product_4_consume_moves = self.production_1.move_raw_ids.filtered(lambda x: x.product_id == self.product_4)
+        product_5_consume_moves = self.production_1.move_raw_ids.filtered(lambda x: x.product_id == self.product_5)
+        self.assertEqual(product_2_consume_moves.product_uom_qty, 2.0)
+        self.assertEqual(product_4_consume_moves.product_uom_qty, 6.0)
+        self.assertEqual(product_5_consume_moves.product_uom_qty, 3.0)
+
+        # create required lots
+        lot_product_2 = self.env['stock.production.lot'].create({'product_id': self.product_2.id})
+        lot_product_4 = self.env['stock.production.lot'].create({'product_id': self.product_4.id})
+
+        # refuel stock
+        inventory = self.env['stock.inventory'].create({
+            'name': 'Inventory For Product C',
+            'filter': 'partial',
+            'line_ids': [(0, 0, {
+                'product_id': self.product_2.id,
+                'product_uom_id': self.product_2.uom_id.id,
+                'product_qty': 30,
+                'prod_lot_id': lot_product_2.id,
+                'location_id': self.ref('stock.stock_location_14')
+            }), (0, 0, {
+                'product_id': self.product_4.id,
+                'product_uom_id': self.product_5.uom_id.id,
+                'product_qty': 60,
+                'prod_lot_id': lot_product_4.id,
+                'location_id': self.ref('stock.stock_location_14')
+            }), (0, 0, {
+                'product_id': self.product_5.id,
+                'product_uom_id': self.product_5.uom_id.id,
+                'product_qty': 60,
+                'location_id': self.ref('stock.stock_location_14')
+            })]
+        })
+        inventory.prepare_inventory()
+        inventory.action_done()
+
+        # re-assign consume material
+        self.production_1.action_assign()
+
+        # Check production order status after assign.
+        self.assertEqual(self.production_1.availability, 'assigned')
+        # Plan production order.
+        self.production_1.button_plan()
+
+        # check workorders
+
+        workorders = self.production_1.workorder_ids
+        # for workorder in workorders:
+        #     print workorder.name, workorder.product_id, workorder.qty_producing
+
+        # first machine (machine A)
+        # self.assertEqual(workorders[0].duration, 40)
+        workorders[0].button_start()
+        finished_lot = self.env['stock.production.lot'].create({'product_id': self.production_1.product_id.id})
+        workorders[0].write({'final_lot_id': finished_lot.id, 'qty_producing': 48})
+
+        product_d_move_lot = workorders[0].active_move_lot_ids.filtered(lambda x: x.product_id == self.product_2)
+        product_d_move_lot.write({'lot_id': lot_product_2.id, 'quantity_done': 2})
+        workorders[0].record_production()
+
+        # Check machine B process....
+        # self.assertEqual(workorders[1].duration, 20, "Workorder duration does not match.")
+        workorders[1].button_start()
+        product_f_move_lot = workorders[1].active_move_lot_ids.filtered(lambda x: x.product_id == self.product_5)
+        product_f_move_lot.write({'lot_id': lot_product_4.id, 'quantity_done': 6})
+        workorders[1].record_production()
+        self.production_1.button_mark_done()
