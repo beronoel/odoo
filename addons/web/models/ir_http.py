73c75adf636cb10474dae0cac451cf90533a62b0
Aaron Bohy <aab@odoo.com>
2016-06-13 11:09:33 +0200

    [ADD] web: put is_admin info into page source

--- addons/web/models/ir_http.py
+++ addons/web/models/ir_http.py
@@ -13,6 +13,7 @@ class Http(models.Model):
         return {
             "session_id": request.session_id,
             "uid": request.session.uid,
+            "is_admin": request.env.user.has_group('base.group_erp_manager'),
             "user_context": request.session.get_context() if request.session.uid else {},
             "db": request.session.db,
             "server_version": version_info.get('server_version'),
