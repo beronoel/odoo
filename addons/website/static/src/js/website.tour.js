c94b04caf8428099550910f1c668fa1aac6ced65
qsm-odoo <qsm@odoo.com>
2016-06-30 15:49:26 +0200

    [TMP] website(_*): use new tour automatic test system

--- addons/website/static/src/js/website.tour.js
+++ addons/website/static/src/js/website.tour.js
@@ -1,38 +1,35 @@
 odoo.define('website.tour.cancel', function (require) {
-'use strict';
-
-var Tour = require('web.Tour');
-Tour.autoRunning = false;
-
+    "use strict";
+    require('web.Tour').autoRunning = false;
 });
 
 odoo.define('website.tour', function (require) {
-'use strict';
+    "use strict";
 
-var Tour = require('web.Tour');
-var website = require('website.website');
-var base = require('web_editor.base');
+    var Tour = require('web.Tour');
+    var tour = require('web_tour.tour');
+    var website = require('website.website');
+    var base = require('web_editor.base');
 
-website.TopBar.include({
-    tours: [],
-    start: function () {
-        var $menu = this.$('#help-menu');
-        setTimeout(function () {
-            _.each(Tour.tours, function (tour) {
-                if (tour.mode === "test") {
-                    return;
-                }
-                var $menuItem = $($.parseHTML('<li><a href="#">'+tour.name+'</a></li>'));
-                $menuItem.click(function () {
-                    Tour.run(tour.id);
+    website.TopBar.include({
+        tours: [],
+        start: function () {
+            var self = this;
+            return this._super.apply(this, arguments).done(function () {
+                var $menu = self.$('#help-menu');
+                _.each(tour.tours, function (tour) {
+                    // if (tour.mode === "test") { FIXME
+                    //     return;
+                    // }
+                    var $menuItem = $($.parseHTML("<li><a href=\"#\">" + tour.id + "</a></li>"));
+                    // $menuItem.click(function () { FIXME
+                    //     tour.run(tour.id);
+                    // });
+                    $menu.append($menuItem);
                 });
-                $menu.append($menuItem);
             });
-        }, 0);
-        return this._super();
-    }
-});
+        }
+    });
 
-base.ready().then(Tour.running);
-
+    base.ready().then(Tour.running);
 });
--- addons/website_crm/static/src/js/website_crm_tour.js
+++ addons/website_crm/static/src/js/website_crm_tour.js
@@ -2,59 +2,45 @@ odoo.define('website_crm.tour', function(require) {
     'use strict';
 
     var base = require('web_editor.base');
-    var core    = require('web.core');
-    var Tour    = require('web.Tour');
-    var Model   = require('web.Model');
-    var Session = require('web.Session');
+    var core = require('web.core');
+    var tour = require('web_tour.tour');
 
-    base.ready().done(function () {
-        Tour.register({
-            id:   'website_crm_tour',
-            name: "Test the contact us form",
-            path: '/page/contactus',
-            mode: 'test',
-            steps: [
-                {
-                    title:          "Complete name",
-                    element:        "input[name=contact_name]",
-                    sampleText:     "John Smith",
-                },
-                {
-                    title:          "Complete phone number",
-                    element:        "input[name=phone]",
-                    sampleText:     "118.218"
-                },
-                {
-                    title:          "Complete Email",
-                    element:        "input[name=email_from]",
-                    sampleText:     "john@smith.com"
-                },
-                {
-                    title:          "Complete Company",
-                    element:        "input[name=partner_name]",
-                    sampleText:     "Odoo S.A."
-                },
-                {
-                    title:          "Complete Subject",
-                    element:        "input[name=name]",
-                    sampleText:     "Useless message"
-                },
-                {
-                    title:          "Complete Subject",
-                    element:        "textarea[name=description]",
-                    sampleText:     "### TOUR DATA ###"
-                },
-                {
-                    title:          "Send the form",
-                    element:        ".o_website_form_send"
-                },
-                {
-                    title:          "Check we were redirected to the success page",
-                    waitFor:        "#wrap:has(h1:contains('Thanks')):has(div.alert-success)"
-                }
-            ]
-        });
+    var _t = core._t;
+
+    base.ready().done(function () { // FIXME mode test
+        tour.register("website_crm_tour", {
+            skip_enabled: true,
+            url: "/page/contactus"
+        }, [{
+            trigger: "input[name=contact_name]",
+            content: _t("Complete name"),
+            sampleText: "John Smith", // FIXME
+        }, {
+            trigger: "input[name=phone]",
+            content: _t("Complete phone number"),
+            sampleText: "118.218" // FIXME
+        }, {
+            trigger: "input[name=email_from]",
+            content: _t("Complete Email"),
+            sampleText: "john@smith.com" // FIXME
+        }, {
+            trigger: "input[name=partner_name]",
+            content: _t("Complete Company"),
+            sampleText: "Odoo S.A." // FIXME
+        }, {
+            trigger: "input[name=name]",
+            content: _t("Complete Subject"),
+            sampleText: "Useless message" // FIXME
+        }, {
+            trigger: "textarea[name=description]",
+            content: _t("Complete Subject"),
+            sampleText: "### TOUR DATA ###" // FIXME
+        }, {
+            trigger: ".o_website_form_send",
+            content: _t("Send the form"),
+        }, {
+            waitFor: "#wrap:has(h1:contains('Thanks')):has(div.alert-success)",
+            content: _t("Check we were redirected to the success page"),
+        }]);
     });
-
-    return {};
 });
