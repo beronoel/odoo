77085bfd8c58c25b6a399b851bb91c7d0fc97225
Ravi Patel <rap@odoo.com>
2016-07-07 16:36:20 +0530

    [IMP] account: User can select 'base currency' as 'account currency' in account Note: -case 1: If the 'Journal Currency' and Currency of Accounts Selected on Journal are different , -case 2: Journal Currency is blank(by default company currency) and If account currency is other than company currency
    the above two cases will raise the error and other cases its works fine

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -412,8 +412,11 @@ class AccountMoveLine(models.Model):
     def _check_currency(self):
         for line in self:
             if line.account_id.currency_id:
-                if not line.currency_id or line.currency_id.id != line.account_id.currency_id.id:
-                    raise ValidationError(_('The selected account of your Journal Entry forces to provide a secondary currency. You should remove the secondary currency on the account.'))
+                if not line.journal_id.currency_id:
+                    if line.company_id.currency_id.id != line.account_id.currency_id.id:
+                        raise ValidationError(_('The selected account of your Journal Entry force to provide a secondary currency. You should  either remove the secondary currency on the account or match the currency of your journal with your account currency.'))
+                elif line.journal_id.currency_id.id != line.account_id.currency_id.id:
+                    raise ValidationError(_('The selected account of your Journal Entry force to provide a secondary currency. You should  either remove the secondary currency on the account or match the currency of your journal with your account currency.'))
 
     @api.multi
     @api.constrains('currency_id', 'amount_currency')
