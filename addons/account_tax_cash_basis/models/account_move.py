1374c04853cf61fcaca82cc61d399f8517926cad
qdp-odoo <qdp@odoo.com>
2016-08-12 16:59:35 +0200

    [WIP] account_tax_cash_basis: report the net and tax amounts of tax in cash basis in the VAT report (at the payment date)

--- addons/account_tax_cash_basis/models/account_move.py
+++ addons/account_tax_cash_basis/models/account_move.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, fields
+from odoo import api, models, fields
 
 
 class AccountMoveCashBasis(models.Model):
@@ -12,3 +12,18 @@ class AccountMoveCashBasis(models.Model):
         string='Tax Cash Basis Entry of',
         help="Technical field used to keep track of the tax cash basis reconciliation."
         "This is needed when cancelling the source: it will post the inverse journal entry to cancel that part too.")
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    @api.model
+    def create(self, vals, apply_taxes=True):
+        tax = False
+        if vals.get('tax_line_id'):
+            tax = self.env['account.tax'].browse(vals['tax_line_id'])
+        if vals.get('tax_ids'):
+            tax = self.env['account.tax'].browse(vals['tax_ids'])
+        if tax and tax.use_cash_basis and not vals.get('tax_exigible'):
+            vals['tax_exigible'] = False
+        return super(AccountMoveLine, self).create(vals, apply_taxes=apply_taxes)
