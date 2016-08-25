72d3decbf99133c6a935417f52656bbde3759a73
Benjamin De Leener <jackbdl@gmail.com>
2016-08-25 12:26:49 -0400

    DEV: members init

--- my-modules/members/controllers/controllers.py
+++ my-modules/members/controllers/controllers.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
 from openerp import http
 
-# class Members(http.Controller):
-#     @http.route('/members/members/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
 
-#     @http.route('/members/members/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('members.listing', {
-#             'root': '/members/members',
-#             'objects': http.request.env['members.members'].search([]),
-#         })
+class Members(http.Controller):
+    @http.route('/members/', auth='public')
+    def index(self, **kw):
+        return "Hello, world"
 
-#     @http.route('/members/members/objects/<model("members.members"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('members.object', {
-#             'object': obj
-#         })
+    @http.route('/members/objects/', auth='public')
+    def list(self, **kw):
+        return http.request.render('members.listing', {
+            'root': '/members',
+            'objects': http.request.env['members.members'].search([]),
+        })
+
+    @http.route('/members/objects/<model("members.members"):obj>/', auth='public')
+    def object(self, obj, **kw):
+        return http.request.render('members.object', {
+            'object': obj
+        })
--- my-modules/members/models/models.py
+++ my-modules/members/models/models.py
@@ -2,14 +2,15 @@
 
 from openerp import models, fields, api
 
-# class members(models.Model):
-#     _name = 'members.members'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class members(models.Model):
+    _name = 'members.members'
+
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+
+    @api.depends('value')
+    def _value_pc(self):
+        self.value2 = float(self.value) / 100
