ca4eeac7504cc1595cf553b9946b7952bdb0d13e
Benjamin De Leener <jackbdl@gmail.com>
2016-08-25 14:17:43 -0400

    DEV: visu members

--- modules/members/controllers/controllers.py
+++ modules/members/controllers/controllers.py
@@ -3,18 +3,18 @@ from openerp import http
 
 
 class Members(http.Controller):
-    @http.route('/members/', auth='public')
+    @http.route('/members/members/', auth='public', website=True)
     def index(self, **kw):
         return "Hello, world"
 
-    @http.route('/members/objects/', auth='public')
+    @http.route('/members/members/objects/', auth='public', website=True)
     def list(self, **kw):
         return http.request.render('members.listing', {
             'root': '/members',
             'objects': http.request.env['members.members'].search([]),
         })
 
-    @http.route('/members/objects/<model("members.members"):obj>/', auth='public')
+    @http.route('/members/members/objects/<model("members.members"):obj>/', auth='public', website=True)
     def object(self, obj, **kw):
         return http.request.render('members.object', {
             'object': obj
