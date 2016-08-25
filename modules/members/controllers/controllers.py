cf5bd6bf72f8009d319d782edddbd1a330c0672a
Benjamin De Leener <jackbdl@gmail.com>
2016-08-25 14:12:27 -0400

    DEV: changes path

--- modules/members/controllers/controllers.py
+++ modules/members/controllers/controllers.py
@@ -3,18 +3,18 @@ from openerp import http
 
 
 class Members(http.Controller):
-    @http.route('/members/members/', auth='public')
+    @http.route('/members/', auth='public')
     def index(self, **kw):
         return "Hello, world"
 
-    @http.route('/members/members/objects/', auth='public')
+    @http.route('/members/objects/', auth='public')
     def list(self, **kw):
         return http.request.render('members.listing', {
             'root': '/members',
             'objects': http.request.env['members.members'].search([]),
         })
 
-    @http.route('/members/members/objects/<model("members.members"):obj>/', auth='public')
+    @http.route('/members/objects/<model("members.members"):obj>/', auth='public')
     def object(self, obj, **kw):
         return http.request.render('members.object', {
             'object': obj
