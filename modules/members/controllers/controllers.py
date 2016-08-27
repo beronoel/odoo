8c9983d41b076023a61acc1171605e2c676b15c6
Benjamin De Leener <jackbdl@gmail.com>
2016-08-27 12:18:17 -0400

    DEV: test

--- modules/members/controllers/controllers.py
+++ modules/members/controllers/controllers.py
@@ -6,16 +6,9 @@ from openerp.http import request
 class Members(http.Controller):
     @http.route('/members/', auth='public')
     def access(self, **kw):
-        cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
+        partner_ids = 'test'
 
-        partner_obj = pool['res.partner.partner_id_membership']
-        partner_ids = partner_obj.search(cr, uid, [], context=context)
         print partner_ids
 
         return http.request.render('members.member_display', partner_ids)
-
-    def display(self):
-
-
-        http.request.render('members.test', "Hello, world")
 
