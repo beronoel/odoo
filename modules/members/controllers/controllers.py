add14bbffa6e78929cf8040132810def3945e8b5
Benjamin De Leener <jackbdl@gmail.com>
2016-08-27 12:19:43 -0400

    DEV: fix

--- modules/members/controllers/controllers.py
+++ modules/members/controllers/controllers.py
@@ -4,7 +4,7 @@ from openerp.http import request
 
 
 class Members(http.Controller):
-    @http.route('/members/', auth='public')
+    @http.route('/members/members/', auth='public')
     def access(self, **kw):
         partner_ids = 'test'
 
