df4f13cc11800db2573e1ad97a892a8d7293e71b
Benjamin De Leener <jackbdl@gmail.com>
2016-08-26 20:08:02 -0400

    DEV: test

--- modules/members/controllers/controllers.py
+++ modules/members/controllers/controllers.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
 from openerp import http
+from openerp.http import request
 
 
 class Members(http.Controller):
-    @http.route('/members/members/', auth='public', website=True)
-    def index(self, **kw):
-        return http.request.render('members.index', "Hello, world")
+    @http.route('/members', type='http', auth='public', website=True)
+    def access(self, **kw):
+        cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
+
+        partner_obj = pool['res.partner.partner_id_membership']
+        partner_ids = partner_obj.search(cr, uid, [], context=context)
+        print partner_ids
+
+
+        return http.request.render('members.display', partner_obj)
+
+    def display(self):
+
+
+        http.request.render('members.test', "Hello, world")
 
