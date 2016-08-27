839376c5d2f912fee6e8b79e01282ccf983c58a2
Benjamin De Leener <jackbdl@gmail.com>
2016-08-27 17:53:58 -0400

    test view

--- modules/members/controllers/controllers.py
+++ modules/members/controllers/controllers.py
@@ -6,6 +6,7 @@ from openerp.http import request
 class Members(http.Controller):
     @http.route('/members/members/', auth='public')
     def access(self, **kw):
+        Teachers = http.request.env['academy.teachers']
 
-        return http.request.render('members.member_display', {'teachers': ["Diana Padilla", "Jody Caroll", "Lester Vaughn"]})
+        return http.request.render('members.member_display', {'teachers': Teachers.search([])})
 
