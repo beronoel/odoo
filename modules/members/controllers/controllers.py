e42f82985e53b5c6cb67f70459e56fe3574e8f29
Benjamin De Leener <jackbdl@gmail.com>
2016-08-28 16:58:46 -0400

    test

--- modules/members/controllers/controllers.py
+++ modules/members/controllers/controllers.py
@@ -7,6 +7,7 @@ class Members(http.Controller):
     @http.route('/members/members/', auth='public')
     def access(self, **kw):
         #Members = http.request.env['res.partner']
+        Members = http.request.env['members.members']
 
-        #return http.request.render('members.member_display', {'members': Members.search([])})
+        return http.request.render('members.member_display', {'members': Members.search([])})
 
