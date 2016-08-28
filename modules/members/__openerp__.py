28075ff177073cdacc2840cd31796d5ab751c776
Benjamin De Leener <jackbdl@gmail.com>
2016-08-28 16:57:28 -0400

    test

--- modules/members/__openerp__.py
+++ modules/members/__openerp__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base_setup', 'hr', 'product', 'account', 'membership'],
+    'depends': ['base', 'base_setup', 'hr', 'product', 'account', 'membership'],
 
     # always loaded
     'data': [
--- modules/members/controllers/controllers.py
+++ modules/members/controllers/controllers.py
@@ -6,7 +6,7 @@ from openerp.http import request
 class Members(http.Controller):
     @http.route('/members/members/', auth='public')
     def access(self, **kw):
-        Members = http.request.env['res.partner']
+        #Members = http.request.env['res.partner']
 
-        return http.request.render('members.member_display', {'members': Members.search([])})
+        #return http.request.render('members.member_display', {'members': Members.search([])})
 
