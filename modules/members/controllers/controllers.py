9dbd1489d9d30b6e0abaca0fc9691ad203a88722
Benjamin De Leener <jackbdl@gmail.com>
2016-08-29 00:08:48 -0400

    render

--- modules/members/controllers/controllers.py
+++ modules/members/controllers/controllers.py
@@ -5,18 +5,16 @@ from wtforms import Form, StringField
 class Members(http.Controller):
     @http.route('/members/members/', website=True)
     def create(self, **post_data):
-        form = SearchForm()
-        return http.request.render('members.search_partner', {'form': form})
-
-    @http.route('/members/search/', type='http', auth='public', methods=['POST'])
-    def search(self, **kw):
         form = SearchForm(http.request.httprequest.form)
-        unique_id = form.unique_id.data  # '29334011400439'
+        if http.request.httprequest.method == 'POST':
+            unique_id = form.unique_id.data  # '29334011400439'
 
-        record_members = http.request.env['res.partner'].sudo()
-        result_record = record_members.search([('partner_id_membership', '=', unique_id)])
+            record_members = http.request.env['res.partner'].sudo()
+            result_record = record_members.search([('partner_id_membership', '=', unique_id)])
 
-        return http.request.render('members.member_display', {'members': result_record})
+            return http.request.render('members.member_display', {'members': result_record})
+
+        return http.request.render('members.search_partner', {'form': form})
 
 class SearchForm(Form):
     unique_id = StringField('unique_id')
