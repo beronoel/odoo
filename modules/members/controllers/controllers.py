61a41c6d2895bab363d15bf1930d795966d12ab4
Benjamin De Leener <jackbdl@gmail.com>
2016-08-28 23:24:01 -0400

    test

--- modules/members/controllers/controllers.py
+++ modules/members/controllers/controllers.py
@@ -7,9 +7,10 @@ class Members(http.Controller):
     @http.route('/members/members/', auth='public')
     def access(self, **kw):
         cr, uid, context = request.cr, request.uid, request.context
-        record_members = http.request.env['res.partner'].search(cr, SUPERUSER_ID, [('name', '=', 'Benjamin De Leener')], context=context)
+        record_members = http.request.env['res.partner']
+
         if not record_members:
             return http.request.render('members.member_display', {'id': 'None', 'name': 'None'})
 
-        return http.request.render('members.member_display', {'members': record_members})
+        return http.request.render('members.member_display', {'members': record_members.search(cr, SUPERUSER_ID, [('name', '=', 'Benjamin De Leener')], context=context)})
 
