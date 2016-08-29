53683f3a42c71eeb91f14e5a7cbef93b9bd51896
Benjamin De Leener <jackbdl@gmail.com>
2016-08-29 10:51:14 -0400

    test scheduler

--- modules/members/models/models.py
+++ modules/members/models/models.py
@@ -16,14 +16,16 @@ class members(models.Model):
     lastModified = fields.Date('Last updated')
 
     def process_scheduler_queue(self, cr, uid, context=None):
-        record_members = http.request.env['res.partner']
+        record_members = self.pool.get(['res.partner'])
+        #record_members = http.request.env['res.partner']
         result_record = record_members.search([('is_in', '=', True)])
 
         for partner in result_record:
             p = record_members.browse(partner.id)
             p.write({'is_in': not partner.is_in})
 
-        scheduler_line_obj = http.request.env['res.partner']
+        record_members = self.pool.get(['members.members'])
+        #record_members = http.request.env['res.partner']
         scheduler_line_ids = scheduler_line_obj.search(cr, uid, [])
         # Loops over every record in the model scheduler.demo
         for scheduler_line_id in scheduler_line_ids:
