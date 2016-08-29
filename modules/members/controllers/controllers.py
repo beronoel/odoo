00ae5eb9250a433630255cdc6c8365799b96cdfc
Benjamin De Leener <jackbdl@gmail.com>
2016-08-28 23:38:03 -0400

    sudo

--- modules/members/controllers/controllers.py
+++ modules/members/controllers/controllers.py
@@ -7,9 +7,10 @@ class Members(http.Controller):
     @http.route('/members/members/', auth='public')
     def access(self, **kw):
         cr, uid, context = request.cr, SUPERUSER_ID, request.context
-        record_members = http.request.env['res.partner']
+        record_members = http.request.env['res.partner'].sudo()
 
-        result_record = record_members.search(cr, uid, [('id', '=', '1')], context=context)
+        result_record = record_members.search([('id', '=', '1')])
+        #result_record = record_members.search(cr, uid, [('id', '=', '1')], context=context)
         print result_record
 
         return http.request.render('members.member_display', {'members': result_record})
