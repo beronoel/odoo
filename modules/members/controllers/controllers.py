2f9549312d91b910d5ca7f755cf17edb82b0f9f9
Benjamin De Leener <jackbdl@gmail.com>
2016-08-28 23:03:42 -0400

    test access search

--- modules/members/controllers/controllers.py
+++ modules/members/controllers/controllers.py
@@ -6,7 +6,8 @@ from openerp.http import request
 class Members(http.Controller):
     @http.route('/members/members/', auth='public')
     def access(self, **kw):
+        cr, uid, context, registry = request.cr, request.uid, request.context, request.registry
         Members = http.request.env['res.partner']
 
-        return http.request.render('members.member_display', {'members': Members.search([[('name','==','Benjamin De Leener')]])})
+        return http.request.render('members.member_display', {'members': Members.search(cr, uid, [('name','==', 'Benjamin De Leener')])})
 
