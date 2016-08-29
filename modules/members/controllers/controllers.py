a290f59f14d2d312e1200a2054359a7ce1cbd655
Benjamin De Leener <jackbdl@gmail.com>
2016-08-28 23:39:38 -0400

    test

--- modules/members/controllers/controllers.py
+++ modules/members/controllers/controllers.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
 from openerp import http
-from openerp.http import request
-from openerp import SUPERUSER_ID
 
 class Members(http.Controller):
     @http.route('/members/members/', auth='public')
     def access(self, **kw):
-        cr, uid, context = request.cr, SUPERUSER_ID, request.context
         record_members = http.request.env['res.partner'].sudo()
 
-        result_record = record_members.search([('id', '=', '1')])
-        #result_record = record_members.search(cr, uid, [('id', '=', '1')], context=context)
-        print result_record
+        result_record = record_members.search([('partner_id_membership', '=', '29334011400439')])
 
         return http.request.render('members.member_display', {'members': result_record})
 
