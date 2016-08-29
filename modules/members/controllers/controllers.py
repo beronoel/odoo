72dc1261f64e73397837b7f0abfcf46ca884181a
Benjamin De Leener <jackbdl@gmail.com>
2016-08-29 00:17:51 -0400

    test

--- modules/members/controllers/controllers.py
+++ modules/members/controllers/controllers.py
@@ -8,14 +8,13 @@ class Members(http.Controller):
         form = SearchForm(http.request.httprequest.form)
         if http.request.httprequest.method == 'POST':
             unique_id = form.unique_id.data  # '29334011400439'
-            print unique_id
 
             record_members = http.request.env['res.partner'].sudo()
             result_record = record_members.search([('partner_id_membership', '=', str(unique_id))])
 
             return http.request.render('members.search_partner', {'form': form, 'members': result_record})
 
-        return http.request.render('members.search_partner', {'form': form})
+        return http.request.render('members.search_partner', {'form': form, 'members': None})
 
 class SearchForm(Form):
     unique_id = StringField('unique_id')
