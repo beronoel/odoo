1256ee077a337d31ef3ad2bfc7071deca6ffeb93
Benjamin De Leener <jackbdl@gmail.com>
2016-08-28 23:41:38 -0400

    remove access

--- modules/members/controllers/controllers.py
+++ modules/members/controllers/controllers.py
@@ -4,9 +4,9 @@ from openerp import http
 class Members(http.Controller):
     @http.route('/members/members/', auth='public')
     def access(self, **kw):
-        record_members = http.request.env['res.partner'].sudo()
 
-        result_record = record_members.search([('partner_id_membership', '=', '29334011400432')])
+        record_members = http.request.env['res.partner'].sudo()
+        result_record = record_members.search([('partner_id_membership', '=', '29334011400439')])
 
         return http.request.render('members.member_display', {'members': result_record})
 
