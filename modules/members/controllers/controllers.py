033921d94ecea476e79e8e2382dd95111fdc38cd
Benjamin De Leener <jackbdl@gmail.com>
2016-08-28 23:14:03 -0400

    test access

--- modules/members/controllers/controllers.py
+++ modules/members/controllers/controllers.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from openerp import http
 from openerp.http import request
-
+import members
 
 class Members(http.Controller):
     @http.route('/members/members/', auth='public')
     def access(self, **kw):
-        cr, uid, context, registry = request.cr, request.uid, request.context, request.registry
-        Members = http.request.env['res.partner']
+        request_members = http.request.env['res.partner']
+        module_members = members()
 
-        return http.request.render('members.member_display', {'members': Members.search([('name', '=', 'Benjamin De Leener')])})
+        return http.request.render('members.member_display', {'members': module_members.search_partner(request_members)})
 
--- modules/members/models/models.py
+++ modules/members/models/models.py
@@ -4,37 +4,38 @@ from odoo import api, models, fields, _
 from odoo.exceptions import UserError
 
 
-#class Partner(models.Model):
-#_inherits = 'res.partner'
+class members(models.Model):
+    _name = 'members.members'
 
-#_name = 'members.members'
+    def search_partner(self, data):
+        return data.search([('name', '=', 'Benjamin De Leener')])
 
-"""
-@api.depends('member_UID')
-def _search_partner(self):
-    # Return info of membership
-    if self.ids:
-        ids_to_search = []
-        for partner in self:
-            if partner.associate_member:
-                ids_to_search.append(partner.associate_member.partner_id_membership)
-            else:
-                ids_to_search.append(partner.partner_id_membership)
-        self.env.cr.execute('''
-                        SELECT
-                            p.id as id,
-                            MIN(m.date_from) as membership_start,
-                            MAX(m.date_to) as membership_stop,
-                            MIN(CASE WHEN m.date_cancel is not null THEN 1 END) as membership_cancel
-                        FROM
-                            res_partner p
-                        LEFT JOIN
-                            membership_membership_line m
-                            ON (m.partner = p.id)
-                        WHERE
-                            p.id IN %s
-                        GROUP BY
-                            p.id''', (tuple(ids_to_search),))
-        for record in self.env.cr.dictfetchall():
-            partner = self.browse(record.pop('id')).update(record)
-"""
+    """
+    @api.depends('member_UID')
+    def _search_partner(self):
+        # Return info of membership
+        if self.ids:
+            ids_to_search = []
+            for partner in self:
+                if partner.associate_member:
+                    ids_to_search.append(partner.associate_member.partner_id_membership)
+                else:
+                    ids_to_search.append(partner.partner_id_membership)
+            self.env.cr.execute('''
+                            SELECT
+                                p.id as id,
+                                MIN(m.date_from) as membership_start,
+                                MAX(m.date_to) as membership_stop,
+                                MIN(CASE WHEN m.date_cancel is not null THEN 1 END) as membership_cancel
+                            FROM
+                                res_partner p
+                            LEFT JOIN
+                                membership_membership_line m
+                                ON (m.partner = p.id)
+                            WHERE
+                                p.id IN %s
+                            GROUP BY
+                                p.id''', (tuple(ids_to_search),))
+            for record in self.env.cr.dictfetchall():
+                partner = self.browse(record.pop('id')).update(record)
+    """
