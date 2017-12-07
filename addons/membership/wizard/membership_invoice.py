3b7604051192d97030e51ebdd6a2cd770e6f3b8b
beronoel <32558133+beronoel@users.noreply.github.com>
2017-12-07 18:02:21 -0500

    update invoice

--- addons/membership/wizard/membership_invoice.py
+++ addons/membership/wizard/membership_invoice.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
+from datetime import timedelta
 import odoo.addons.decimal_precision as dp
 
 
